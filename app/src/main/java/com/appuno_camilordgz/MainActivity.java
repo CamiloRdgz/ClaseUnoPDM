8ae22293f04a5379132409f3d529bc9dd92a1aa4
Camilo Rodriguez Gomez <crodriguezg006@gmail.com>
2024-02-06 09:12:40 -0500

    Variable Creation
    Some variables about personal characteristics are created.

--- app/src/main/java/com/appuno_camilordgz/MainActivity.java
+++ app/src/main/java/com/appuno_camilordgz/MainActivity.java
@@ -6,6 +6,11 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    String name = "Camilo Rodriguez Gomez";
+    int age = 17;
+    char gender = 'M';
+    float height = (float)1.78;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
