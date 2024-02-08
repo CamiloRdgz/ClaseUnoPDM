3663892e29ce1f57f632c0d9644ef75397373031
Camilo Rodriguez Gomez <crodriguezg006@gmail.com>
2024-02-08 09:28:32 -0500

    Basic App
    TextVIews and Buttons are created and basic actions and attributes are added

--- app/src/main/java/com/appuno_camilordgz/MainActivity.java
+++ app/src/main/java/com/appuno_camilordgz/MainActivity.java
@@ -11,9 +11,34 @@ public class MainActivity extends AppCompatActivity {
     char gender = 'M';
     float height = (float)1.78;
 
+    TextView username;
+    Button btn1, btn2;  
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        username = findViewById(R.id.user);
+        btn1 = findViewById(R.id.btn1);
+        btn2 = findViewById(R.id.btn2);
+
+        btn1.setOnClickListener( new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                username.setText("Button 1 Pressed");
+                Toast.makeText(this, "Button 1 Pressed", Toast.LENGTH_SHORT);
+            }
+        })
+
+        btn2.setOnClickListener( new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                username.setText("Button 2 Pressed")
+                Toast.makeText(this, "Button 2 Pressed", Toast.LENGTH_SHORT);
+            }
+        })
     }
 }
